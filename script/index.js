9b428c856eb621fdea06ed5bfc7aca5b3ac5f890
Silence520 <1056497547@qq.com>
2019-01-01 19:15:49 +0800

    更新特殊符号 操作问题

--- script/index.js
+++ script/index.js
@@ -1,13 +1,10 @@
 (function() {
     var root = (typeof self == 'object' && self.self === self && self) || (typeof global == 'object' && global.global === global && global) || this || {};
-    // console.log(root);
-    // Create a safe reference to the Underscore object for use below.
     var Calves = function(obj) {
         if (obj instanceof Calves) return obj;
         if (!(this instanceof Calves)) return new Calves(obj);
         this.Calveswrapped = obj;
     };
-
     if (typeof exports != 'undefined' && !exports.nodeType) {
         if (typeof module != 'undefined' && !module.nodeType && module.exports) {
             exports = module.exports = Calves;
@@ -19,39 +16,39 @@
 
     // Calves version.
     Calves.VERSION = '1.0.0';
-
     Calves.reg = '';
     var character = {
-        number: '0-9',
+        number: '\\d',
         capitalCode: 'a-z',
         lowercaseCode: 'A-Z',
         matchingChinese: '\u4E00-\u9FA5'
     };
-
+    // 简便方式
     Calves.quick = function(obj) {
         let quickUser = {
             Number: `^[0-9]*$`, //数字
             Chinese: `^[\u4e00-\u9fa5]*$`, //中文
-            IDCard: `^\d{8,18}|[0-9x]{8,18}|[0-9X]{8,18}?$`, //身份证号
+            IDCard: `^\\d{8,18}|[0-9x]{8,18}|[0-9X]{8,18}?$`, //身份证号
             Letter: `^[A-Za-z]+$ `, //字母
             LetterNumber: `^[A-Za-z0-9]+$`, //字母数字
             LowerCaseEnglish: `^[a-z]+$`, //小写英文
             CapitalEnglish: `^[A-Z]+$`, //大写英文
-            Phone: `^1[35789]d{9}$`, // :匹配手机号
+            Phone: `^1[35789]\\d{9}$`, // :匹配手机号
             Email: `^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$`, //Email，
             Month: `^(0?[1-9]|1[0-2])$`, //月份
             Days: '^((0?[1-9])|((1|2)[0-9])|30|31)$' //日
         };
 
         if (!quickUser[obj]) {
-            throw '1004   正则匹配不存在！';
-            return false;
+            throw Error('1004   正则匹配不存在！');
         } else {
             return new RegExp(quickUser[obj]);
         }
     };
+
     // init 初始化数据
     Calves.init = function(obj) {
+        // console.log(JSON.stringify(obj));
         let initVariable = {
             strictStart: false,
             strictEnding: false,
@@ -61,17 +58,12 @@
         Calves.reg = '';
         let newobj = Object.assign(initVariable, obj); //merge 数组
         if (newobj.isglobal && newobj.strictStart) {
-            throw '1000  全局匹配和开头匹配不能同时使用';
-            return false;
+            throw Error('1000 全局匹配和开头匹配不能同时使用');
         }
-
         if (newobj.isglobal && newobj.strictEnding) {
-            throw '1001 全局匹配和结尾匹配不能同时使用';
-            return false;
+            throw Error('1001 全局匹配和结尾匹配不能同时使用');
         }
-
         matching(newobj);
-
         return resultValue(newobj);
     };
 
@@ -79,44 +71,36 @@
     var matching = function(prames) {
         Calves.reg += prames.strictStart ? '^' : '';
         if (prames.children.length < 1) {
-            throw '1002 匹配参数不能为空！';
-            return false;
+            throw Error('1002 匹配参数不能为空！');
         }
         //循环对子元素进行操作赋值
         prames.children.map(item => {
             if (JSON.stringify(item) === '{}') {
-                throw '1003 存在空对象!'; // 如果为空,返回false
-                return false;
+                throw Error('1003 存在空对象!'); // 如果为空,返回false
             }
-            // 判断用[] 还是 ()
-            var isBrackets =
-                item.customCharacter &&
-                item.customCharacter.some(item => {
-                    if (item.length >= 3 || (item.length >= 2 && item.slice(0, 1) !== '/')) {
-                        return true;
-                    } else {
-                        return false;
+            let isBrackets = false;
+            let items = item.customCharacter;
+            if (items) {
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i].length >= 2 && items[i].slice(0, 2) !== '\\') {
+                        isBrackets = true;
                     }
-                });
-
+                }
+            }
             if (isBrackets) {
                 Calves.reg += item.singleMatch ? '(?:' : '('; //是否需要单独匹配
             } else {
                 Calves.reg += item.isReversal ? '[^' : '['; //判断是否取反
             }
-
             item.number && constant('number');
             item.capitalCode && constant('capitalCode');
             item.lowercaseCode && constant('lowercaseCode');
             item.matchingChinese && constant('matchingChinese');
             //自定义字符
             item.customCharacter && item.customCharacter.length > 0 && custom(item.customCharacter, isBrackets);
-
             Calves.reg += isBrackets ? ')' : ']';
-
             matchingTimes(item);
         });
-
         Calves.reg += prames.strictEnding ? '$' : '';
     };
 
@@ -141,19 +125,11 @@
 
     //返回值结果
     var resultValue = function(prames) {
-        switch (prames) {
-            case prames.isglobal && prames.isignore:
-                return new RegExp(Calves.reg, 'ig');
-                break;
-            case prames.isglobal:
-                return new RegExp(Calves.reg, 'g');
-                break;
-            case prames.isignore:
-                return new RegExp(Calves.reg, 'i');
-                break;
-            default:
-                return new RegExp(Calves.reg);
-                break;
+        if (prames.isglobal || prames.isignore) {
+            let pra = prames.isignore ? 'i' : '' + prames.isglobal ? 'g' : '';
+            return new RegExp(Calves.reg, pra);
+        } else {
+            return new RegExp(Calves.reg);
         }
     };
 })();
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import Calves from '../script/index.js';
-describe('测试纯数字', function() {
+describe('测试 init() 方法', function() {
     test('验证数字', () => {
         let num = {
             strictStart: true,
@@ -15,6 +15,7 @@ describe('测试纯数字', function() {
         let errorNum = 'A';
         expect(regNum.test(testNum)).toBe(true);
         expect(regNum.test(errorNum)).toBe(false);
+        expect(regNum.test('4')).toBe(true);
     });
 
     test('验证全局匹配', function() {
@@ -31,6 +32,43 @@ describe('测试纯数字', function() {
         let codeglobelerror = '1212';
         expect(regcodeglobel.test(codeglobelok)).toBe(true);
         expect(regcodeglobel.test(codeglobelerror)).toBe(false);
+        expect(regcodeglobel.test('12a12')).toBe(true);
+    });
+
+    test('全局验证大小写字母', function() {
+        let codeall = {
+            isignore: true,
+            isglobal: true,
+            children: [
+                {
+                    customCharacter: ['a']
+                }
+            ]
+        };
+        let regcodeall = Calves.init(codeall);
+        let codeallok = '122a';
+        let codeallerror = '234F4';
+        expect(regcodeall.test(codeallok)).toBe(true);
+        expect(regcodeall.test(codeallerror)).toBe(false);
+        expect(regcodeall.test('122A')).toBe(true);
+    });
+
+    test('验证全局匹配大小写', function() {
+        let codeignore = {
+            isignore: true,
+            children: [
+                {
+                    customCharacter: ['a']
+                }
+            ]
+        };
+        let regcodeignore = Calves.init(codeignore);
+        let codeignoreok = 'a';
+        let codeignoreok2 = 'A';
+        expect(regcodeignore.test(codeignoreok)).toBe(true);
+        expect(regcodeignore.test(codeignoreok2)).toBe(true);
+        expect(regcodeignore.test('B')).toBe(false);
+        expect(regcodeignore.test('b')).toBe(false);
     });
 });
 
@@ -56,9 +94,11 @@ describe('测试手机号码', function() {
         let testPhone = '13838624806';
         let errorPhone = '23838624806';
         let errorPhone2 = '12838624806';
+        let testPhone2 = '15878457896';
         expect(regPhone.test(testPhone)).toBe(true);
         expect(regPhone.test(errorPhone)).toBe(false);
         expect(regPhone.test(errorPhone2)).toBe(false);
+        expect(regPhone.test(testPhone2)).toBe(true);
     });
 });
 
@@ -68,20 +108,76 @@ describe('测试匹配图片', function() {
             strictEnding: true,
             children: [
                 {
-                    customCharacter: ['/.']
+                    customCharacter: ['.']
                 },
                 {
                     customCharacter: ['png', 'gif', 'jpe?g']
                 }
             ]
         };
+
         let regImg = Calves.init(objImg);
         let testImg = '.png';
         let testImg1 = '.jpg';
         let errorImg = '.pnng';
         expect(regImg.test(testImg)).toBe(true);
-        expect(regImg.test(testImg1)).toBe(true);
         expect(regImg.test(errorImg)).toBe(false);
+        expect(regImg.test(testImg1)).toBe(true);
+    });
+});
+
+describe('测试init()中文', function() {
+    test('测试中文', () => {
+        let objChinese = {
+            children: [
+                {
+                    matchingChinese: true,
+                    minCount: 1,
+                    maxCount: 4
+                }
+            ]
+        };
+        let regChinese = Calves.init(objChinese);
+        let testChinese = '中文';
+        let testChinese1 = '匹配中文';
+        let errorChinese = 'hello world';
+        expect(regChinese.test(testChinese)).toBe(true);
+        expect(regChinese.test(errorChinese)).toBe(false);
+        expect(regChinese.test(testChinese1)).toBe(true);
+    });
+});
+
+// describe('测试init()错误', function() {
+//     test('错误catch', () => {
+//         let objChinese = {
+//             strictStart: true,
+//             strictEnding: true,
+//             isglobal: true,
+//             children: [{}]
+//         };
+//         let regChinese = Calves.init(objChinese);
+//         expect(regChinese).toThrow('1000 全局匹配和开头匹配不能同时使用');
+//     });
+// });
+
+describe('测试特定的字符', function() {
+    test('特定字符', () => {
+        let objspec = {
+            strictEnding: true,
+            strictStart: true,
+            children: [
+                {
+                    number: true,
+                    customCharacter: ['a', 'b', 'c'],
+                    minCount: 2,
+                    maxCount: 4
+                }
+            ]
+        };
+        let regobjspec = Calves.init(objspec);
+        expect(regobjspec.test('1122')).toBe(true);
+        expect(regobjspec.test('1A31a')).toBe(false);
+        expect(regobjspec.test('1a1a')).toBe(true);
     });
 });
 
@@ -92,6 +188,7 @@ describe('测试快捷方式', function() {
         let quickError = 'hello world';
         expect(quick.test(quickOk)).toBe(true);
         expect(quick.test(quickError)).toBe(false);
+        expect(quick.test('10001')).toBe(true);
     });
 
     test('快捷方式 中文', () => {
@@ -100,6 +197,7 @@ describe('测试快捷方式', function() {
         let quickError = 'hello world';
         expect(quick.test(quickOk)).toBe(true);
         expect(quick.test(quickError)).toBe(false);
+        expect(quick.test('世界')).toBe(true);
     });
 
     test('快捷方式 月份', () => {
@@ -108,5 +206,54 @@ describe('测试快捷方式', function() {
         let quickError = '13';
         expect(quick.test(quickOk)).toBe(true);
         expect(quick.test(quickError)).toBe(false);
+        expect(quick.test('5')).toBe(true);
+    });
+    test('验证手机号', () => {
+        let quickPhone = Calves.quick('Phone');
+        let testPhone = '13838624801';
+        let errorPhone = '23838624806';
+        let errorPhone2 = '12838624806';
+        let testPhone2 = '15878457896';
+        expect(quickPhone.test(testPhone)).toBe(true);
+        expect(quickPhone.test(errorPhone)).toBe(false);
+        expect(quickPhone.test(errorPhone2)).toBe(false);
+        expect(quickPhone.test(testPhone2)).toBe(true);
+    });
+    test('验证身份证号', () => {
+        let quickPhone = Calves.quick('IDCard');
+        let testIDCard = '412727199512032221';
+        let errorIDCard = 'AS';
+        expect(quickPhone.test(testIDCard)).toBe(true);
+        expect(quickPhone.test(errorIDCard)).toBe(false);
+    });
+    test('验证 字母数字大小写', () => {
+        let quickLetter = Calves.quick('LetterNumber');
+        let testIDCard = '41';
+        let errorIDCard = 'AS';
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test('@#$%^&*')).toBe(false);
+    });
+    test('验证 字母数字大写', () => {
+        let quickLetter = Calves.quick('CapitalEnglish');
+        let testIDCard = 'AS';
+        let errorIDCard = 'as';
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test('@#$%^&*')).toBe(false);
+    });
+    test('验证 字母数字小写', () => {
+        let quickLetter = Calves.quick('LowerCaseEnglish');
+        let testIDCard = 'as';
+        let errorIDCard = 'AS';
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test(quickLetter)).toBe(false);
+        expect(quickLetter.test('@#$%^&*')).toBe(false);
+    });
+});
+
+describe('测试版本信息', function() {
+    test('验证 测试版本信息', () => {
+        expect(Calves.VERSION).toBe('1.0.0');
     });
 });
